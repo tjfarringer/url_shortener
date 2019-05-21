3f11e1246f7c93f964ab39c9a2cc8981ed8f0d1d
Talmadge Farringer <tjfarrin@gmail.com>
2019-05-20 20:50:58 -0400

    fix the cors issue

--- server/index.js
+++ server/index.js
@@ -1,3 +1,5 @@
+
+
 const mongoose = require("mongoose");
 const express = require("express");
 const bodyParser = require('body-parser')
@@ -33,6 +35,11 @@ const port = process.env.PORT || 3005;
 const app = express();
 
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 // parse application/x-www-form-urlencoded
